9eb8c77a4ab95acde0b5d480c64669fdcef99860
pengxiao <pengxiao@squarevalleytech.com>
2022-12-27 15:43:59 +0800

    update

--- hello-world/hello.js
+++ hello-world/hello.js
@@ -1,16 +1,20 @@
 // Lambda function code
 
 module.exports.handler = async (event) => {
-  console.log('Event: ', event);
-  let responseMessage = 'Hello, World!';
+    console.log('Event: ', event);
+    let responseMessage = 'Hello, World!';
 
-  return {
-    statusCode: 200,
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      message: responseMessage,
-    }),
-  }
+    if (event.queryStringParameters && event.queryStringParameters['Name']) {
+        responseMessage = 'Hello, ' + event.queryStringParameters['Name'] + '!';
+    }
+
+    return {
+        statusCode: 200,
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            message: responseMessage,
+        }),
+    }
 }
